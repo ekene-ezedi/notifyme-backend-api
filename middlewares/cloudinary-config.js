5d4cb810fe8c3470662b8fe6d332bf44b8b5ec5e
ekene-ezedi <ezedi.eo.ekene@gmail.com>
2020-08-18 21:56:27 +0100

    parse cloudinary api env

--- middlewares/cloudinary-config.js
+++ middlewares/cloudinary-config.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 
 const cloudinaryConfig = (req,res,next)=>{
     cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_API_SECRET,
+        cloud_name: JSON.parse(process.env.CLOUDINARY_CLOUD_NAME),
+        api_key: JSON.parse(process.env.CLOUDINARY_API_KEY),
+        api_secret: JSON.parse(process.env.CLOUDINARY_API_SECRET),
     });
 
     next();
