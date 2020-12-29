e8b9c39cbed70f04fffc523e10a297ae4f0f2341
ekene-ezedi <ezedi.eo.ekene@gmail.com>
2020-12-29 21:23:23 +0100

    use custom cors setup

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ const cloudinaryConfig = require("./middlewares/cloudinary-config")
 const app = express();
 const enforce = require("express-sslify");
 
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://stark-spire-56927.herokuapp.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
 // app.use(
 //   cors({
@@ -19,14 +37,14 @@ app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cookieParser());
 app.use("/public", express.static("public"));
 app.use("*", cloudinaryConfig);
-app.options(
-  "*",
-  cors({
-    origin: "https://stark-spire-56927.herokuapp.com",
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
+// app.options(
+//   "*",
+//   cors({
+//     origin: "https://stark-spire-56927.herokuapp.com",
+//     optionsSuccessStatus: 200,
+//     credentials: true,
+//   })
+// );
 //essential parts
 require("./startup/db")();
 require("./startup/routes")(app);
