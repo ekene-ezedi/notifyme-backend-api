8e6e0155cb53c920ab037f628eeb952701897781
ekene-ezedi <ezedi.eo.ekene@gmail.com>
2020-12-29 16:05:55 +0100

    fix cors

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 //define packages/modules
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const cloudinaryConfig = require('./middlewares/cloudinary-config').cloudinaryConfig;
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const cloudinaryConfig = require("./middlewares/cloudinary-config")
+  .cloudinaryConfig;
 const app = express();
-const enforce = require('express-sslify');
+const enforce = require("express-sslify");
 
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
-app.use(cors({origin:true,credentials:true}));
+app.use(
+  cors({
+    origin: "https://stark-spire-56927.herokuapp.com/",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
-app.use('/public',express.static('public'));
-app.use('*',cloudinaryConfig);
+app.use("/public", express.static("public"));
+app.use("*", cloudinaryConfig);
 //essential parts
-require('./startup/db')();
-require('./startup/routes')(app);
+require("./startup/db")();
+require("./startup/routes")(app);
 
 //listen to server
 const port = process.env.PORT || 3000;
-const server = app.listen(port, ()=>console.log(`App running on port ${port}....`));
+const server = app.listen(port, () =>
+  console.log(`App running on port ${port}....`)
+);
 module.exports = server;
